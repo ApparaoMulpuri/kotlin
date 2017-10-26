c56d346069859c942703f3dc1089da2eb4b48a14
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-10-26 18:41:57 +0300

    To add diagnostics

--- compiler/testData/diagnostics/tests/inference/implicitInvokeInObjectWithFunctionalArgument.kt
+++ compiler/testData/diagnostics/tests/inference/implicitInvokeInObjectWithFunctionalArgument.kt
@@ -7,8 +7,8 @@ object TestClass {
 
 fun test(s: String): String {
     val a = TestClass { TestClass { TestClass } }
-    a <!UNRESOLVED_REFERENCE!>checkType<!> { <!UNRESOLVED_REFERENCE!>_<!><TestClass>() }
+    a <!NI;UNRESOLVED_REFERENCE!><!UNRESOLVED_REFERENCE!>checkType<!><!> { <!NI;UNRESOLVED_REFERENCE!><!UNRESOLVED_REFERENCE!>_<!><!><TestClass>() }
 
-    <!UNREACHABLE_CODE!>val b =<!> TestClass { return s }
-    <!UNREACHABLE_CODE!>b <!UNRESOLVED_REFERENCE!>checkType<!> { <!UNRESOLVED_REFERENCE!>_<!><Nothing>() }<!>
+    <!NI;UNREACHABLE_CODE!><!UNREACHABLE_CODE!>val b =<!><!> TestClass { return s }
+    <!NI;UNREACHABLE_CODE!><!UNREACHABLE_CODE!>b <!NI;UNRESOLVED_REFERENCE!><!UNRESOLVED_REFERENCE!>checkType<!><!> { <!NI;UNRESOLVED_REFERENCE!><!UNRESOLVED_REFERENCE!>_<!><!><Nothing>() }<!><!>
 }
